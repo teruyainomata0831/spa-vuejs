a056ed555ae69ac299862673e9432377b3c61bbe
猪股輝哉 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-04-17 17:25:26 +0900

    spa-vuejs

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <router-view name="header"></router-view>
+    <transition name="fade" mode="out-in" @before-enter="beforeEnter">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    beforeEnter () {
+      this.$route.$emit('triggerScroll')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-actie {
+    transition: opacity 0.5s;
+  }
+</style>
---
+++ src/router.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Users from './views/Users.vue'
+import UsersPosts from './views/UsersPosts.vue'
+import UsersProfile from './views/UsersProfile.vue'
+import HeaderHome from './views/HeaderHome.vue'
+import HeaderUsers from './views/HeaderUsers.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [{path: '/', components: {
+    default: Home,
+    header: HeaderHome
+  }},
+  {path: '/users/:id', components: {
+    default: Users,
+    header: HeaderUsers
+  },
+  props: {
+    default: true,
+    header: false
+  },
+  children: [
+    {path: 'posts', component: UsersPosts},
+    {path: 'profile', component: UsersProfile, name: 'users-id-pofile'}]
+  }, {
+    path: '*',
+    redirect: {path: '/'}
+  }
+],
+  scrollBehavior(to, from, savedPostion) {
+    return new Promise(resolve => {
+      this.app.$root.$once('triggerScroll', () => {
+        let position = { x: 0, y: 100 }
+        if (savedPostion) {
+          position = savedPostion;
+        }
+        if (to.hash) {
+          position = {
+            selector: to.hash
+          }
+        }
+        resolve(position)
+      })
+    })
+  }
+});
